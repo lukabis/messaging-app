f326dc522259b1c9a2e8e46700086cb5ce396c83
Luka <LukaBis@lbiskupic05@gmail.com>
2026-01-12 18:50:59 +0100

    feat: deskop design

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import LoginView from "./views/LoginView";
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="
+        max-w-7xl
+        mx-auto
+        bg-[#292929]
+        rounded-none lg:rounded-xl
+    ">
       <LoginView />
     </div>
   );
