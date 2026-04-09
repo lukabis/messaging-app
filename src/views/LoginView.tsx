1859fb5b6d7b8fc6c186405ed79e96c607208e98
Luka <LukaBis@lbiskupic05@gmail.com>
2026-04-10 00:39:52 +0200

    feat: redirecting user to onboarding view if not onboarded

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import ChatRound from '../components/chat-round/chat-round';
 import GoogleSignInButton from '../components/google-sign-in-button/google-sign-in-button';
@@ -6,15 +7,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function LoginView() {
   const { loginWithRedirect, logout, isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!isAuthenticated) return;
 
     const callProtected = async () => {
       const token = await getAccessTokenSilently();
-      await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/user`, {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/user`, {
         headers: { Authorization: `Bearer ${token}` },
       });
+      const data = await res.json();
+      if (!data.onboarded) navigate("/onboarding");
     };
 
     callProtected();
