b4fee3c32e3224f3fe4fbc0fffe2bc2ebf78d4dc
Luka <LukaBis@lbiskupic05@gmail.com>
2026-04-11 23:58:02 +0200

    feat: add animated route transitions with framer-motion slide+fade

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import LoginView from "./views/LoginView";
 import OnboardingView from "./views/OnboardingView";
 import HomeView from "./views/HomeView";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
   return (
-    <BrowserRouter>
-      <div className="max-w-7xl mx-auto bg-[#292929] rounded-none lg:rounded-xl">
-        <Routes>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.25, ease: "easeInOut" }}
+      >
+        <Routes location={location}>
           <Route path="/" element={<LoginView />} />
           <Route element={<ProtectedRoute />}>
             <Route path="/onboarding" element={<OnboardingView />} />
             <Route path="/home" element={<HomeView />} />
           </Route>
         </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="max-w-7xl mx-auto bg-[#292929] rounded-none lg:rounded-xl">
+        <AnimatedRoutes />
       </div>
     </BrowserRouter>
   );
